7ece4a8112c9028d0a8362d9da91a44fc9c12c6a
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-08-19 16:30:03 -0300

    Módulo Funcionario finalizado

---
+++ Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+namespace LocadoraDeAutomoveis.Dominio.ModuloFuncionario
+
+public class Funcionario : EntidadeBase
+{
+    public string Nome { get; set; }
+    public DateTime DataAdmissao { get; set; }
+    public float Salario { get; set; }
+    
+    public Funcionario()
+    {
+         
+    }
+
+    public Funcionario(string nome, DateTime dataAdmissao, float salario)
+    {
+        Nome = nome;
+        DataAdmissao = dataAdmissao;
+        Salario = salario;
+    }
+}
---
+++ LocadoraDeAutomoveis.Web/Controllers/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeAutomoveis.Web.Controllers
+{
+    public class Funcionario : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
