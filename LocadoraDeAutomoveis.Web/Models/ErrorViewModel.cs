5d076cd947414c21d0895956785c7dc757978d83
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-08-19 14:11:13 -0300

    Add project files.

---
+++ LocadoraDeAutomoveis.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocadoraDeAutomoveis.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
