b41303211106a430f4b3e87d4a584ac01d9bc4bc
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-08-19 16:38:41 -0300

    Módulo GrupoAutomoveis criado

---
+++ GrupoAutomoveis.cs
@@ -0,0 +1,12 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+namespace LocadoraDeAutomoveis.Dominio.ModuloGrupoAutomoveis
+
+public class GrupoAutomoveis : EntidadeBase
+{
+	public string Nome { get; set; }
+
+	public GrupoAutomoveis(string nome)
+	{
+		Nome = nome;
+	}
+}
