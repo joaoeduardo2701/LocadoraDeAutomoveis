cd4536a96bfd075cc6cd6ac0397516ad66dfc29f
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-08-19 17:07:45 -0300

    modulo grupoAutomoveis e Irepositorio criados

---
+++ LocadoraDeAutomoveis.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+
+public interface IRepositorio<TEntidade> where TEntidade : EntidadeBase
+{
+    void Inserir(TEntidade entidade);
+    void Editar(TEntidade entidadeAtualizada);
+    void Excluir(TEntidade entidadeParaExcluir);
+    TEntidade? SelecionarPorId(int idSelecionado);
+    List<TEntidade> SelecionarTodos();
+}
---
+++ LocadoraDeAutomoveis.Dominio/ModuloGrupoAutomoveis/GrupoAutomoveis.cs
@@ -0,0 +1,14 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+
+namespace LocadoraDeAutomoveis.Dominio.ModuloGrupoAutomoveis
+{
+    public class GrupoAutomoveis : EntidadeBase
+    {
+        public string Nome { get; set; }
+
+        public GrupoAutomoveis(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
