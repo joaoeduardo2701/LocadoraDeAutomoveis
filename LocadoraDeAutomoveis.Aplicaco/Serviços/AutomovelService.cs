0b4b5912f682b1fdd3d01d3b6d55b2bfa63f21ce
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-08-28 16:04:09 -0300

    camada de aplicação finalizada

---
+++ LocadoraDeAutomoveis.Aplicaco/Serviços/AutomovelService.cs
@@ -0,0 +1,69 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Dominio.ModuloAutomovel;
+
+namespace LocadoraDeAutomoveis.Aplicacao.Serviços
+{
+    public class AutomovelService
+    {
+        private readonly IRepositorioAutomovel repositorioAutomovel;
+
+        public AutomovelService(IRepositorioAutomovel repositorioAutomovel)
+        {
+            this.repositorioAutomovel = repositorioAutomovel;
+        }
+
+        public Result<Automovel> Inserir(Automovel automovel)
+        {
+			repositorioAutomovel.Inserir(automovel);
+
+	        return Result.Ok(automovel);
+        }
+
+        public Result<Automovel> Editar(Automovel automovelAtualizado)
+        {
+	        var automovel = repositorioAutomovel.SelecionarPorId(automovelAtualizado.Id);
+
+	        if (automovel is null)
+		        return Result.Fail("O veículo não foi encontrado!");
+
+	        automovel.Modelo = automovelAtualizado.Modelo;
+	        automovel.Marca = automovelAtualizado.Marca;
+	        automovel.TipoCombustivel = automovelAtualizado.TipoCombustivel;
+	        automovel.CapacidadeTanque = automovelAtualizado.CapacidadeTanque;
+	        automovel.GrupoAutomoveisId = automovelAtualizado.GrupoAutomoveisId;
+
+			repositorioAutomovel.Editar(automovel);
+
+	        return Result.Ok(automovel);
+        }
+
+        public Result<Automovel> Excluir(int automovelId)
+        {
+	        var veiculo = repositorioAutomovel.SelecionarPorId(automovelId);
+
+	        if (veiculo is null)
+		        return Result.Fail("O veículo não foi encontrado!");
+
+	        repositorioAutomovel.Excluir(veiculo);
+
+	        return Result.Ok(veiculo);
+        }
+
+        public Result<Automovel> SelecionarPorId(int automovelId)
+        {
+	        var veiculo = repositorioAutomovel.SelecionarPorId(automovelId);
+
+	        if (veiculo is null)
+		        return Result.Fail("O veículo não foi encontrado!");
+
+	        return Result.Ok(veiculo);
+        }
+
+        public Result<List<Automovel>> SelecionarTodos()
+        {
+	        var veiculos = repositorioAutomovel.SelecionarTodos();
+
+	        return Result.Ok(veiculos);
+        }
+	}
+}
