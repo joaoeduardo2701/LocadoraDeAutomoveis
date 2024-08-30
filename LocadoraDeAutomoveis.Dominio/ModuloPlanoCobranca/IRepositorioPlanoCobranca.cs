86fcfffe7816dd76f1200a1b9a5f8fa4b18eb06a
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-08-30 18:02:17 -0300

    Domínio do plano cobrança finalizado

---
+++ LocadoraDeAutomoveis.Dominio/ModuloPlanoCobranca/IRepositorioPlanoCobranca.cs
@@ -0,0 +1,9 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+
+namespace LocadoraDeAutomoveis.Dominio.ModuloPlanoCobranca
+{
+    public interface IRepositorioPlanoCobranca : IRepositorio<PlanoCobranca>
+    {
+
+    }
+}
---
+++ LocadoraDeAutomoveis.Dominio/ModuloPlanoCobranca/PlanoCobranca.cs
@@ -0,0 +1,52 @@
+﻿using LocadoraDeAutomoveis.Dominio.Compartilhado;
+using LocadoraDeAutomoveis.Dominio.ModuloGrupoAutomoveis;
+
+namespace LocadoraDeAutomoveis.Dominio.ModuloPlanoCobranca
+{
+    public class PlanoCobranca : EntidadeBase 
+    {
+        public int GrupoVeiculosId { get; set; }
+        public GrupoAutomoveis GrupoAutomoveis { get; set; }
+
+        public decimal PrecoDiarioPlanoDiario { get; set; }
+        public decimal PrecoQuilometroPlanoDiario { get; set; }
+
+        public decimal QuilometrosDisponiveisPlanoControlado { get; set; }
+        public decimal PrecoDiarioPlanoControlado { get; set; }
+        public decimal PrecoQuilometroExtrapoladoPlanoControlado { get; set; }
+
+        public decimal PrecoDiarioPlanoLivre { get; set; }
+
+        public PlanoCobranca(
+            int grupoVeiculosId,
+            decimal precoDiarioPlanoDiario,
+            decimal precoQuilometroPlanoDiario,
+            decimal quilometrosDisponiveisPlanoControlado,
+            decimal precoDiarioPlanoControlado,
+            decimal precoQuilometroExtrapoladoPlanoControlado,
+            decimal precoDiarioPlanoLivre
+        )
+        {
+            GrupoVeiculosId = grupoVeiculosId;
+
+            PrecoDiarioPlanoDiario = precoDiarioPlanoDiario;
+            PrecoQuilometroPlanoDiario = precoQuilometroPlanoDiario;
+
+            QuilometrosDisponiveisPlanoControlado = quilometrosDisponiveisPlanoControlado;
+            PrecoDiarioPlanoControlado = precoDiarioPlanoControlado;
+            PrecoQuilometroExtrapoladoPlanoControlado = precoQuilometroExtrapoladoPlanoControlado;
+
+            PrecoDiarioPlanoLivre = precoDiarioPlanoLivre;
+        }
+
+        public override List<string> Validar()
+        {
+            List<string> erros = [];
+
+            if (GrupoVeiculosId == 0)
+                erros.Add("O grupo de veículos é obrigatório");
+
+            return erros;
+        }
+    }
+}
