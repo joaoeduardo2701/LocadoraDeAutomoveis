e09ea2f542369caa5b0105634dbb85851eb268b0
joaoeduardo2701 <duducardoso2701@gmail.com>
2024-09-01 23:17:10 -0300

    Adicionadas as views

--- LocadoraDeAutomoveis/Controllers/PlanoCobrancaController.cs
+++ LocadoraDeAutomoveis/Controllers/PlanoCobrancaController.cs
@@ -9,5 +9,10 @@ namespace LocadoraDeAutomoveis.WebApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Inserir()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
